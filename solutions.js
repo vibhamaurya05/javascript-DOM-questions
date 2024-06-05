5db4bc129afe823fe0a191e4b372d2215a93fce8
vibhamaurya05 <vibhamaurya5sep@gmail.com>
2024-06-05 17:47:14 +0530

    javascript DOM practice

---
+++ solutions.js
@@ -0,0 +1,80 @@
+/*******question 1*******/
+
+document.querySelector('.crayons-subtitle-2').innerHTML = 'Vibha Maurya'
+
+document.querySelector('.color-base-70').innerHTML = 'My name is vibha maurya'
+
+
+
+/*******question 3*******/
+
+
+let row = document.createElement('section');
+row.setAttribute('class','parent')
+const h2 = document.createElement('h2');
+let text = document.createTextNode('My new FAQs');
+h2.appendChild(text);
+row.appendChild(h2);
+const newrow = document.querySelector('.accordion-homepage');
+newrow.appendChild(row);
+
+
+
+/*******question 5*******/
+
+document.querySelector('.productmylist .buynow').innerHTML = "Check out";
+
+
+/*******question 6*******/
+
+
+let search = document.querySelector('._header_container_1spbu_30 ._header_bottom_558sn_1 input');
+
+search.addEventListener('onmouseover', () => {
+
+    search.style.backgroundColor = 'red';
+
+});
+
+
+
+/*******question 9*******/
+let heading = document.querySelector('.header_viewport-80 .padding-section-medium h1')
+
+heading.style.fontFamily = "Impact,Charcoal,sans-serif";
+
+heading.style.color = "black";
+
+
+/*******question 11*******/
+document.querySelector('.header-main .wrapper .logo ').style.backgroundImage ="url('https://neurolab.ineuron.ai/images/ineuron-logo-white.png')";
+
+
+
+/*******question 12*******/
+
+document.querySelector('.FormControl-spacingWrapper button').style.backgroundColor = "blue";
+
+/*******question 14*******/
+
+document.querySelector('.HotDealsAll__HotCampaignsEventsContainer__FK0V2 .HotDealsAll__Heading__2fIbe').style.fontSize = "80px";
+
+
+/*******question 15*******/
+document.querySelector('#gn5535796vn002orb1 .ps-top .ps-system-title-container .ps-title').style.textAlign = "right";
+
+
+/*******question 18*******/
+
+document.querySelector('.p-layout-row .p-grid-item').style.backgroundColor = "orange";
+
+
+/*******question 19*******/
+let pic = document.querySelector('.navbar__container .container a img').src;
+
+
+/*******question 20*******/
+
+document.querySelector('.homepage-featured-product-container .homepage-featured-product-bottom .ft-body-2').style.color = "blue";
+
+
